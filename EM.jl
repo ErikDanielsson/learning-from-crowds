eac124716e94d16bb1b3dfd9de885a6faac748c9
Erik Danielsson <danielsson.erik.0@gmail.com>
2023-11-13 14:59:03 +0100

    Reformat

--- EM.jl
+++ EM.jl
@@ -1,20 +1,13 @@
-import numpy as np
-import random
-import pandas as pd
-import matplotlib.pyplot as plt
-
-random.seed(123)
-
-
-def generate_data(N, w):
+function generate_data(N, w)
     x1 = np.random.uniform(0, 1, N)
     x2 = np.random.uniform(0, 1, N)
-    x = np.array([x1, x2]).T
+    x = [x1, x2]
     y = 1 * (np.dot(x, w) > 0)
     return x, y
+end
 
 
-def expert_advice(y, alpha, beta):
+function expert_advice(y, alpha, beta)
     N = len(y)
     M = len(alpha)
     advice = np.zeros((N, M))
@@ -22,10 +15,15 @@ def expert_advice(y, alpha, beta):
         if yi == 1:
             for j, a in enumerate(alpha):
                 advice[i, j] = 1 * (np.random.uniform(0, 1) <= a)
+            end
         else:
             for j, b in enumerate(beta):
                 advice[i, j] = 1 * (np.random.uniform(0, 1) >= b)
+            end
+        end
+    end
     return advice
+end
 
 
 def sigmoid(x):
@@ -45,59 +43,53 @@ def H(w, X, N):
     )
 
 
-def EM(X, y, epsilon_tot, epsilon_log):
+def EM(X, advice):
     N, n_features = X.shape
-    R = y.shape[1]
+    n_experts = advice.shape[1]
     w = np.zeros(n_features)
     # Majority voting for initalization
-    mu = np.zeros(N)
-    l_obs_prev = -10000000
-    for i in range(N):
-        mu[i] = 1 / R * sum(y[i, :])
-
-    alpha = np.zeros(R)
-    beta = np.zeros(R)
-    for j in range(R):
-        alpha[j] = sum(mu[i] * y[i, j] for i in range(N)) / sum(mu)
-        beta[j] = sum((1 - mu[i]) * (1 - y[i, j]) for i in range(N)) / (N - sum(mu))
-    eta = 0.01
-    for _ in range(10000):
+    alpha = np.random.rand(n_experts)
+    beta = np.random.rand(n_experts)
+    eta = np.ones(n_features)
+    for _ in range(100):
         # E-step
         p = np.zeros(N)
         a = np.zeros(N)
         b = np.zeros(N)
+        mu = np.zeros(N)
         for i in range(N):
             p[i] = sigmoid(np.dot(w, X[i, :]))
             a[i] = np.prod(
-                [alpha[j] if y[i, j] == 1 else 1 - alpha[j] for j in range(R)]
+                [
+                    alpha[j] if advice[i, j] == 1 else 1 - alpha[j]
+                    for j in range(n_experts)
+                ]
             )
-            b[i] = np.prod([beta[j] if y[i, j] == 0 else 1 - beta[j] for j in range(R)])
+            b[i] = np.prod(
+                [
+                    beta[j] if advice[i, j] == 0 else 1 - beta[j]
+                    for j in range(n_experts)
+                ]
+            )
             mu[i] = a[i] * p[i] / (a[i] * p[i] + b[i] * (1 - p[i]))
-        l_obs = sum(np.log((a[i] * p[i]) + b[i] * (1 - p[i])) for i in range(N))
-        if l_obs - l_obs_prev < epsilon_tot:
-            break
-        l_obs_prev = l_obs
 
         # M-step
         # For the analytical parameters
-        for j in range(R):
-            alpha[j] = sum(mu[i] * y[i, j] for i in range(N)) / sum(mu)
-            beta[j] = sum((1 - mu[i]) * (1 - y[i, j]) for i in range(N)) / (N - sum(mu))
+        for j in range(n_experts):
+            alpha[j] = sum(mu[i] * advice[i, j] for i in range(N)) / sum(mu)
+            beta[j] = sum((1 - mu[i]) * (1 - advice[i, j]) for i in range(N)) / (
+                N - sum(mu)
+            )
         # Newton-Raphson for the logistic regression
-        for _ in range(1000):
-            h = H(w, X, N)
-            new_w = w - eta * np.linalg.inv(h) @ g(w, mu, X, N)
-            new_w /= np.linalg.norm(new_w)
-            if np.linalg.norm(new_w - w) < epsilon_log:
-                break
-            w = new_w
-    w /= w[0]
+        for i in range(10):
+            w = w - eta @ np.linalg.inv(H(w, X, N)) @ g(w, mu, X, N)
+
     return alpha, beta, w
 
 
-w = np.array([1, -2])
-x, y = generate_data(1000, w)
-advice = expert_advice(y, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
+w = np.array([1, -1])
+x, y = generate_data(100, w)
+advice = expert_advice(y, [1, 1, 1], [1, 1, 1])
 positive = np.array([[x1, x2] for (x1, x2), yi in zip(x, y) if yi == 1])
 negative = np.array([[x1, x2] for (x1, x2), yi in zip(x, y) if yi == 0])
 plt.scatter(positive[:, 0], positive[:, 1])
@@ -106,5 +98,5 @@ rot90 = np.array([[0, -1], [1, 0]])
 l = rot90 @ w
 plt.plot(np.linspace(0, 1, 100), l[1] / l[0] * np.linspace(0, 1, 100))
 plt.show()
-alpha, beta, w = EM(x, advice, 1e-6, 1e-6)
+alpha, beta, w = EM(x, advice)
 print(alpha, beta, w)
