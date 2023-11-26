e9840b7dcae70a35f3fed8861f60c78c7dfb2b46
K Dull <kdulleru@usc.edu>
2023-11-26 19:17:56 +0100

    update yan-yan-et-al.py

--- yan-yan-et-al.py
+++ yan-yan-et-al.py
@@ -33,25 +33,61 @@ def calc_p_tilde(xi, yi, v, a):
     return p
 
 
-# should we change this ^ to be flexible to bernoulli or gaussian?
+def g(w, mu, X, N):
+    return sum((mu[i] - sigmoid(np.dot(w, X[i, :]))) * X[i, :] for i in range(N))
 
 
-def fit_classifier():
-    pass
+def H(w, X, N):
+    return -sum(
+        sigmoid(np.dot(w, X[i, :]))
+        * (1 - sigmoid(np.dot(w, X[i, :])))
+        * np.outer(X[i, :], X[i, :])
+        for i in range(N)
+    )
 
 
-def fit_annotator():
-    pass
+def soft_lab(yit, p_tilde):
+    return yit * p_tilde + (1-yit) * (1-p_tilde)
 
 
-def EM(x, y):
+def EM(x, y, epsilon_tot, epsilon_log):
     N, D = x.shape
     N, T = y.shape
     p_tilde = np.zeros(N)
+    soft_label = np.zeros(N,T)
     v = np.zeros(T, D + 1)
     a = np.zeros(D + 1)
-    while True:
+    a_new = np.empty(D + 1, dtype = float)
+    a_new.fill(1000)
+    gamma = 0.01
+    while ((np.linalg.norm(a)- np.linalg.norm(a_new))^2 > epsilon_tot):
+        a = a_new
+        # E-step
         for i in range(N):
             xi = x[i, :]
             yi = y[i, :]
             p_tilde[i] = calc_p_tilde(xi, yi, v, a)
+            p_ti = p_tilde[i]
+            for t in range(T):
+                yit = yi[t]
+                soft_label[i,t] = soft_lab(yit, p_ti)
+        
+        # M-step
+        for _ in range(1000):
+            h = H(a, x, N)
+            new_a = a - gamma * np.linalg.inv(h) @ g(a, p_tilde, x, N)
+            new_a /= np.linalg.norm(new_a)
+            if np.linalg.norm(new_a - a) < epsilon_log:
+                break
+            a_new = new_a
+        a_new /= a_new[0]
+        for t in range(T):
+            for _ in range(1000):
+                h = H(v[t, :], x, N)
+                new_v = v[t, :] - gamma * np.linalg.inv(h) @ g(v[t, :], soft_label[:, t], x, N)
+                new_v /= np.linalg.norm(new_v)
+                if np.linalg.norm(new_v - v[t, :]) < epsilon_log:
+                    break
+                v[t, : ] = new_v
+            v[t, :] /= v[t, 0]
+    return a, v
