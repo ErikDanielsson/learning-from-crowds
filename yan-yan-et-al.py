495ff8b5390092b1e6fbd1786218ea0f031dadd3
Erik Danielsson <danielsson.erik.0@gmail.com>
2023-11-28 20:45:45 +0100

    Do even more stuff

--- yan-yan-et-al.py
+++ yan-yan-et-al.py
@@ -20,39 +20,29 @@ def expert_advice(y, x, w):
         if yi == 1:
             for j in range(M):
                 advice[i, j] = 1 * (
-                    np.random.uniform(0, 1)
-                    >= sigmoid(np.dot(w[0:2, j], x[i, :]) + w[-1, j])
+                    np.random.uniform(0, 1) <= dot_sigmoid(x[i, :], w[:, j])
                 )
         else:
             for j in range(M):
                 advice[i, j] = 1 * (
-                    np.random.uniform(0, 1)
-                    >= sigmoid(np.dot(w[0:2, j], x[i, :]) + w[-1, j])
+                    np.random.uniform(0, 1) >= dot_sigmoid(x[i, :], w[:, j])
                 )
     return advice
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    if x < -500:
+        return 0
+    return 1 / (1 + np.exp(-x))
 
 
 # We define v = [w \gamma]
 def dot_sigmoid(x, v):
-    return sigmoid(np.dot(x, v[:-1]) + v[-1])
+    return sigmoid(np.dot(x, v))
 
 
 def yit_estimate(yit, xi, v):
     return 1 - dot_sigmoid(xi, v) if yit == 0 else dot_sigmoid(xi, v)
-
-
-def yit_est_gauss(yit, xi, v):
-    sigma = dot_sigmoid(xi, v)
-    const = 1 / (sigma * np.sqrt(2 * np.pi))
-    return (
-        const * np.exp(1 / sigma ^ 2)
-        if y == 1
-        else const * np.exp(-1 / (2 * sigma ^ 2))
-    )
 
 
 def calc_p_tilde(xi, yi, v, a):
@@ -60,24 +50,17 @@ def calc_p_tilde(xi, yi, v, a):
     z_factor = dot_sigmoid(xi, a)
     for t in range(len(yi)):
         yit = yi[t]
-        p *= yit_estimate(yit, xi, v[t, :]) * z_factor
-    return p
-
-
-def g(w, mu, X, N):
-    return sum((mu[i] - sigmoid(np.dot(w, X[i, :]))) * X[i, :] for i in range(N))
-
-
-def H(w, X, N):
-    return -sum(
-        sigmoid(np.dot(w, X[i, :]))
-        * (1 - sigmoid(np.dot(w, X[i, :])))
-        * np.outer(X[i, :], X[i, :])
-        for i in range(N)
-    )
+        e = yit_estimate(yit, xi, v[t, :])
+        p *= e
+    return p * z_factor
 
 
 def log_loss(w, y, x):
+    s = 0
+    for i in range(len(y)):
+        sigma = dot_sigmoid(x[i, :], w)
+        s += y[i] * np.log(sigma) + (1 - y[i]) * np.log(1 - sigma)
+    return -s
     return -sum(
         y[i] * np.log(dot_sigmoid(x[i, :], w))
         + (1 - y[i]) * np.log(1 - dot_sigmoid(x[i, :], w))
@@ -86,8 +69,8 @@ def log_loss(w, y, x):
 
 
 def gradient_log_loss(w, y, x):
-    x_1 = np.hstack((x, np.ones((x.shape[0], 1))))
-    return -sum(x_1[i, :] * (y[i] - dot_sigmoid(x[i, :], w)) for i in range(len(y)))
+    # x_1 = np.hstack((x, np.ones((x.shape[0], 1))))
+    return -sum(x[i, :] * (y[i] - dot_sigmoid(x[i, :], w)) for i in range(len(y)))
 
 
 def soft_lab(yit, p_tilde):
@@ -99,12 +82,12 @@ def EM(x, y, epsilon_tot, epsilon_log):
     N, T = y.shape
     p_tilde = np.zeros(N)
     soft_label = np.zeros((N, T))
-    v = np.zeros((T, D + 1))
-    a = np.zeros(D + 1)
-    a_new = np.empty(D + 1, dtype=float)
-    a_new.fill(1)
+    v = np.random.rand(T, D)
+    a = np.zeros(D)
+    a_new = np.ones(D)
+    a_new[1] = -1
     gamma = 0.01
-    x_1 = np.hstack((x, np.ones((N, 1))))
+    x_1 = x  # np.hstack((x, np.ones((N, 1))))
     while abs(np.linalg.norm(a) - np.linalg.norm(a_new)) > epsilon_tot:
         a = a_new
         # E-step
@@ -112,10 +95,17 @@ def EM(x, y, epsilon_tot, epsilon_log):
             xi = x[i, :]
             yi = y[i, :]
             p_tilde[i] = calc_p_tilde(xi, yi, v, a)
+        # p_tilde /= sum(p_tilde)
+
+        for i in range(N):
             p_ti = p_tilde[i]
             for t in range(T):
                 yit = yi[t]
                 soft_label[i, t] = soft_lab(yit, p_ti)
+        print(sum(p_tilde))
+        print((soft_label[3, 0]))
+        print((soft_label[3, 1]))
+        print((soft_label[3, 2]))
 
         # M-step
         a_new = scipy.optimize.minimize(
@@ -126,6 +116,7 @@ def EM(x, y, epsilon_tot, epsilon_log):
             method="BFGS",
         ).x
         a_new /= a_new[0]
+        # a_new /= np.linalg.norm(a_new)
         for t in range(T):
             v[t, :] = scipy.optimize.minimize(
                 log_loss,
@@ -134,47 +125,44 @@ def EM(x, y, epsilon_tot, epsilon_log):
                 args=(soft_label[:, t], x),
                 method="BFGS",
             ).x
-            v[t, :] /= v[t, 0]
+            # v[t, :] /= np.linalg.norm(v[t, :])
 
         print(a)
         print(v)
-        """
-        for _ in range(1000):
 
-            h = H(a, x_1, N)
-            new_a = a - gamma * np.linalg.inv(h) @ g(a, p_tilde, x_1, N)
-            new_a /= np.linalg.norm(new_a)
-            if np.linalg.norm(new_a - a) < epsilon_log:
-                break
-            a_new = new_a
-        a_new /= a_new[0]
-        for t in range(T):
-            for _ in range(1000):
-                h = H(v[t, :], x_1, N)
-                new_v = v[t, :] - gamma * np.linalg.inv(h) @ g(
-                    v[t, :], soft_label[:, t], x_1, N
-                )
-                new_v /= np.linalg.norm(new_v)
-                if np.linalg.norm(new_v - v[t, :]) < epsilon_log:
-                    break
-                v[t, :] = new_v
-            v[t, :] /= v[t, 0]
-        """
     return a, v
 
 
 w_real = np.array([1, -2])
-x, y = generate_data(10000, w_real)
-advice = expert_advice(y, x, np.array([[1, 1, 1], [1, 1, 1], [1, 1, 200]]))
-a, v = EM(x, advice, 1e-8, 1e-8)
+x, y = generate_data(1000, w_real)
+advice = expert_advice(y, x, np.array([[10, -10], [2000, 2000], [2000, 2000]]).T)
+a, v = EM(x, advice, 1e-8, 1e-6)
 print(a, v)
+fig, axs = plt.subplots(2, 2)
+
 positive = np.array([[x1, x2] for (x1, x2), yi in zip(x, y) if yi == 1])
 negative = np.array([[x1, x2] for (x1, x2), yi in zip(x, y) if yi == 0])
-plt.scatter(positive[:, 0], positive[:, 1])
-plt.scatter(negative[:, 0], negative[:, 1])
+axs[0, 0].scatter(positive[:, 0], positive[:, 1])
+axs[0, 0].scatter(negative[:, 0], negative[:, 1])
+positive = []
+negative = []
+for t in range(3):
+    positive.append(
+        np.array([[x1, x2] for (x1, x2), yi in zip(x, advice[:, t]) if yi == 1])
+    )
+    negative.append(
+        np.array([[x1, x2] for (x1, x2), yi in zip(x, advice[:, t]) if yi == 0])
+    )
+axs[1, 0].scatter(positive[0][:, 0], positive[0][:, 1])
+axs[1, 0].scatter(negative[0][:, 0], negative[0][:, 1])
+axs[0, 1].scatter(positive[1][:, 0], positive[1][:, 1])
+axs[0, 1].scatter(negative[1][:, 0], negative[1][:, 1])
+axs[1, 1].scatter(positive[2][:, 0], positive[2][:, 1])
+axs[1, 1].scatter(negative[2][:, 0], negative[2][:, 1])
+
 rot90 = np.array([[0, -1], [1, 0]])
 l_real = rot90 @ w_real
 l_est = rot90 @ a[0:2]
-plt.plot(np.linspace(0, 1, 100), l_real[1] / l_real[0] * np.linspace(0, 1, 100))
-plt.plot(np.linspace(0, 1, 100), l_est[1] / l_est[0] * np.linspace(0, 1, 100))
+axs[0, 0].plot(np.linspace(0, 1, 100), l_real[1] / l_real[0] * np.linspace(0, 1, 100))
+axs[0, 0].plot(np.linspace(0, 1, 100), l_est[1] / l_est[0] * np.linspace(0, 1, 100))
 plt.show()
