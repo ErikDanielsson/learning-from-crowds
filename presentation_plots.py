c111e420a472d0833fbda1cbc28f4062f604d8a3
Erik Danielsson <danielsson.erik.0@gmail.com>
2023-12-04 16:34:35 +0100

    Move plot creation to separate file

---
+++ presentation_plots.py
@@ -0,0 +1,87 @@
+import random
+from yan_yan_et_al import *
+from plotting import *
+
+random.seed(1234)
+
+a_real = np.array([1, -2, 0])
+x, y = generate_data(1000, a_real)
+x = x[:, 0:-1]
+v_real = np.array(
+    [
+        [10, -3, 1],
+        [10, 10, -10],
+        [4, 4, -2],
+        [0, -4, 2],
+    ]
+)
+advice = expert_advice(y, x, v_real)
+
+x_1 = np.column_stack((x, np.ones(x.shape[0])))
+
+plt.figure()
+plot_bin_datapoints(x, y, plt.gca(), marker="o", markersize=20)
+plt.gcf().suptitle("Ground truth", fontsize=16)
+plt.gca().set_xlabel("Feature 1")
+plt.gca().set_ylabel("Feature 2")
+plt.savefig("yan-yan-et-al-ground-truth.png")
+
+annotator_inds = [(0, 0), (1, 0), (0, 1), (1, 1)]
+
+fig, axs = plt.subplots(2, 2)
+for i, ind in enumerate(annotator_inds):
+    plot_bin_datapoints(x, advice[:, i], axs[*ind], marker="o")
+    axs[*ind].set_title(f"Expert {i + 1}")
+    axs[*ind].set_xlabel("Feature 1")
+    axs[*ind].set_ylabel("Feature 2")
+fig.suptitle("Expert predictions", fontsize=16)
+fig.tight_layout()
+plt.savefig("yan-yan-et-al-expert-predictions.png")
+
+
+fig, axs = plt.subplots(2, 2)
+for i, ind in enumerate(annotator_inds):
+    conf = create_color_classifier(x_1, v_real[i, :])
+    plot_cont_datapoints(x, conf, axs[*ind], marker="o")
+    axs[*ind].set_title(f"Expert {i + 1}")
+    axs[*ind].set_xlabel("Feature 1")
+    axs[*ind].set_ylabel("Feature 2")
+fig.suptitle("Expert bias", fontsize=16)
+fig.tight_layout()
+plt.savefig("yan-yan-et-al-expert-bias.png")
+
+
+a, v = EM(x, advice, 1e-3, 1e-6)
+print(a, v)
+
+plt.figure()
+x_1 = np.column_stack((x, np.ones(x.shape[0])))
+pred_confidence = create_color_classifier(x_1, a, threshold=0.5)
+plot_bin_datapoints(x, pred_confidence, plt.gca(), markersize=20)
+plt.gcf().suptitle("Estimated ground truth", fontsize=16)
+plt.gca().set_xlabel("Feature 1")
+plt.gca().set_ylabel("Feature 2")
+plt.savefig("yan-yan-et-al-estimated-ground-truth.png")
+
+
+fig, axs = plt.subplots(2, 2)
+for i, ind in enumerate(annotator_inds):
+    conf = create_color_annotator(x_1, pred_confidence, v[i, :], 0.5)
+    plot_bin_datapoints(x, conf, axs[*ind])
+    axs[*ind].set_title(f"Expert {i + 1}")
+    axs[*ind].set_xlabel("Feature 1")
+    axs[*ind].set_ylabel("Feature 2")
+fig.suptitle("Estimated expert predictions", fontsize=16)
+fig.tight_layout()
+plt.savefig("yan-yan-et-al-estimated-expert-predictions.png")
+
+fig, axs = plt.subplots(2, 2)
+for i, ind in enumerate(annotator_inds):
+    conf = create_color_classifier(x_1, v[i, :])
+    plot_cont_datapoints(x, conf, axs[*ind])
+    axs[*ind].set_title(f"Expert {i + 1}")
+    axs[*ind].set_xlabel("Feature 1")
+    axs[*ind].set_ylabel("Feature 2")
+fig.suptitle("Estimated expert bias", fontsize=16)
+fig.tight_layout()
+plt.savefig("yan-yan-et-al-estimated-expert-bias.png")
